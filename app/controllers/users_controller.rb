7afece97a96a009406a5251a88dbf17c2d545a67
SzeChan <chansd5@gmail.com>
2015-02-22 20:37:37 -0500

    Hartl 9.51

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   # Filter only applies to :edit and :update actions
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   
-  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show 
     @user = User.find(params[:id])
   end
