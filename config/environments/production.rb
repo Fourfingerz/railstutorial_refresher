aed76350cc760d2f28290246aceec9f3ac0269d5
SzeChan <chansd5@gmail.com>
2015-03-19 16:36:34 -0400

    Specified Host URL in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'hartlfakeapp.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: "hartlfakeapp.herokuapp.com" }
   ActionMailer::Base.smtp_settings = {
     :addresses      => 'smtp.sendgrid.net',
     :port           => '587',
